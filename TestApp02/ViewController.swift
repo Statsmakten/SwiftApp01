e0e946353e430b3250c27e96bf95f389e8b79e81
David Andersson Ljungberg <David@davids-macbook-pro.local>
2017-12-27 10:17:13 +0100

    Initial Commit

---
+++ TestApp02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApp02
+//
+//  Created by David Andersson Ljungberg on 27/12/17.
+//  Copyright © 2017 testOrg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
